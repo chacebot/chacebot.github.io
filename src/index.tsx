eed7589276d1223de21ff9dd1e3f84d8e06c6317
Chace Medeiros <cfmedeir@ncsu.edu>
2024-01-16 18:21:39 -0500

    Add build code

--- src/index.tsx
+++ src/index.tsx
@@ -6,21 +6,21 @@ import "bootstrap/dist/css/bootstrap.css";
 import { HashRouter } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
 // ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </React.StrictMode>,
 //   document.getElementById("root")
 // );
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
