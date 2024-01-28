27f5982e87d4b1dfc8936e7f6e1957dcf35cda6f
Chace Medeiros <cfmedeir@ncsu.edu>
2024-01-28 09:19:04 -0500

    Added poop detection

--- src/Content/Professional/ProfessionalContent.ts
+++ src/Content/Professional/ProfessionalContent.ts
@@ -1,11 +1,12 @@
 import { SurgicalRobot } from "./SurgicalRobot/SurgicalRobot"
 import { Serve } from "./Serve/Serve"
 import { Zippy } from "./Zippy/Zippy"
+import {PoopDetection} from "./PoopDetection/PoopDetection"
 import { PageInfo } from "../../Common/ContentStructure"
 import professional from "./professional.md"
 
 
 export const ProfessionalContent: PageInfo = {
     summary: professional,
-    content: [SurgicalRobot, Serve, Zippy]
+    content: [SurgicalRobot, PoopDetection, Serve, Zippy]
 }
